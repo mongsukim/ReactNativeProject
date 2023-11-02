ea13904335dd2a7109f0f25047156c8138874121
HyeSu <developermong2@gmail.com>
2023-11-02 18:49:20 +0900

    Feat : Core Components를 이용해 UI작성해보기

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,11 +1,20 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { StatusBar } from 'expo-status-bar'
 
 const MainScreen = () => {
   return (
-    <View>
-      <Text>MainScreen</Text>
-    </View>
+    <SafeAreaView>
+      <StatusBar barstyle={'default'}/>
+      <Text>TODO App</Text>
+      <View>
+        <Text>할 일</Text>
+      </View>
+      <View/>
+      <View>
+        <Text>완료된 일</Text>
+      </View>
+    </SafeAreaView>
   )
 }
 
